979ec0d8349f0acc14cac6d084df4ac6aa11a9fd
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-08-01 12:32:47 +0300

    improve profileRepository

--- src/main/java/org/patsimas/happy/repositories/ProfileRepository.java
+++ src/main/java/org/patsimas/happy/repositories/ProfileRepository.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.patsimas.happy.domain.Profile;
 import org.patsimas.happy.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
+	@Query(value = "SELECT * FROM check_happiness.profiles WHERE user_id = :userId", 
+			nativeQuery = true)
 	List<Profile> findProfileByUserId(Long userId);
 }
